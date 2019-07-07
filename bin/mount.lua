478e6b86b39c1e08c5a60d914ad74c7ab50e1e11
UTFTymoteusz <tymoteusz.paliwoda@gmail.com>
2019-07-07 21:06:31 +0200

    mount works

---
+++ bin/mount.lua
@@ -0,0 +1,14 @@
+local fs = require('fs')
+local sh = require('sh')
+local res = require('res')
+
+local args = sh.getArgs()
+
+if #args ~= 2 then
+    stderr:writeln('mount: Invalid amount of arguments')
+    return
+end
+local s, code = fs.mount(args[1], args[2])
+if not s then
+    stderr:writeln('mount: ' .. res.translate(code))
+end
