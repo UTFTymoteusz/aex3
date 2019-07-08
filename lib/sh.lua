c65343d36a45052f4a5839f2a4faf2c7576b7c33
UTFTymoteusz <tymoteusz.paliwoda@gmail.com>
2019-07-08 02:13:46 +0200

    lib/sh: sh.parseArgs() function

--- lib/sh.lua
+++ lib/sh.lua
@@ -23,4 +23,32 @@ function sh.getTTYSize()
     local x, y = io.getstdout():getSize()
     return x or 0, y or 0
 end
+function sh.parseArgs(args, skipEmpty)
+    local tbl = {options = {}, args = {}}
+    local i = 0
+    
+    while (i < #args) do
+        i = i + 1  
+        local v = args[i]
+
+        if (string.sub(v, 1, 2) == '--') then
+            local nv = args[i + 1]
+            if nv then
+                if (nv[1] == '-') then nv = true
+                else i = i + 1 end
+            else nv = true end
+            
+            tbl.options[v] = nv
+        elseif (v[1] == '-') then
+            nv = args[i + 1]
+            if nv then
+                if (nv[1] == '-') then nv = true
+                else i = i + 1 end
+            else nv = true end
+            
+            for i = 2, #v do tbl.options['-' .. v[i] ] = nv end
+        elseif (#v ~= 0) then table.add(tbl.args, {v}) end
+    end
+    return tbl
+end
 return sh
