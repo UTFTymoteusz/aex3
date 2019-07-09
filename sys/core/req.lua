ab29cdf1759397567881221d36658166040aa252
UTFTymoteusz <tymoteusz.paliwoda@gmail.com>
2019-07-10 00:51:49 +0200

    I think I fixed an vulnerability

--- sys/core/req.lua
+++ sys/core/req.lua
@@ -4,12 +4,13 @@ local aex_int = sys.get_internal_table()
 function require(lib)
     if not lib then return nil end
 
-    local lib_paths = {
-        lib,
-        lib .. '.lib',
-        '/lib/' .. lib,
-        '/lib/' .. lib .. '.lib',
-    }
+    local lib_paths = {}
+
+    if string.sub(lib, 1, 2) == './' then
+        table.add(lib_paths, {lib, lib .. '.lib'}) end
+
+    table.add(lib_paths, {'/lib/' .. lib, '/lib/' .. lib .. '.lib'})
+
     local fd = nil
 
     for _, p in pairs(lib_paths) do
